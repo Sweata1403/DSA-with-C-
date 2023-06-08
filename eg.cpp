f6480753167708d77d3cce908a5d2fafa4c6f008
Sweata Chakraborty <85730840+Sweata1403@users.noreply.github.com>
2023-06-08 22:17:18 +0530

    Add files via upload

---
+++ eg.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+
+    int num = 0;
+
+
+
+    while (num < 10)
+
+    {
+
+        if (num == 5)
+
+        break;
+
+        num++;
+
+    }
+
+    cout << num;
+
+}
